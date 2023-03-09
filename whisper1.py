d510d080483ab5b28ad6c3e2dc9b87cfe414a060
Andres Perez <andres.antonio2000@hotmail.com>
2023-03-09 16:21:55 -0300

    GPT v0.0.1

--- whisper1.py
+++ whisper1.py
@@ -1,7 +1,7 @@
 import openai
 import os
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 def listening(audioMp3):
@@ -22,16 +22,16 @@ def resume(text):
 
 
 
-print(resume(listening("audios/audioAng2.mp3")))
-# if __name__ == "__main__":
-#     nombre = input("Nombre del archivo: ")
-#     nombre_ex = "audios/"+nombre
-#     path = os.getcwd()
-#     file_path = os.path.join(path+"/AudiosTXT", f"{nombre}.txt")
-#     with open(file_path, "w", encoding="UTF-8") as file:
-#         text = listening(nombre_ex)
-#         file.write(text)
-#     file_path = os.path.join(path+"/resumenes", f"{nombre}_resumen.txt")
+# print(resume(listening("audios/audioAng2.mp3")))
+if __name__ == "__main__":
+    nombre = input("Nombre del archivo con extension: ")
+    nombre_ex = "audios/"+nombre
+    path = os.getcwd()
+    file_path = os.path.join(path+"/AudiosTXT", f"{nombre}.txt")
+    with open(file_path, "w", encoding="UTF-8") as file:
+        text = listening(nombre_ex)
+        file.write(text)
+    file_path = os.path.join(path+"/resumenes", f"{nombre}_resumen.txt")
 
-#     with open(file_path, "w", encoding="UTF-8") as res:
-#         res.write(str(resume(text)))
+    with open(file_path, "w", encoding="UTF-8") as res:
+        res.write(str(resume(text)))
